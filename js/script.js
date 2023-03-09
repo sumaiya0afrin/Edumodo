9d4191deb03aca6df843478876d3578c7376ae2b
Sumaiya Afrin <106167790+sumaiya0afrin@users.noreply.github.com>
2023-03-10 00:18:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$("#counter1").countMe(70,10);
+$("#counter2").countMe(180,150);
+$("#counter3").countMe(50,5);
+
+
+
+wow = new WOW(
+    {
+      animateClass: 'animated',
+      offset:       100,
+      callback:     function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+      }
+    }
+  );
+  wow.init();
+  document.getElementById('moar').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+  };
+
+
